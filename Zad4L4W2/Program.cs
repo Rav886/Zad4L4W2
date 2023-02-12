549fd9afee65eefc3b132a2099be0755e5e6f260
Ra v <timon521182@hotmail.com>
2023-02-12 21:57:43 +0100

    Add project files.

---
+++ Zad4L4W2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a ;
+            string b ;
+            double c ;
+
+            a = 10 ;
+            b = "Szkoła Dotneta";
+            c = 12.5 ;
+        }
+    }
+}
